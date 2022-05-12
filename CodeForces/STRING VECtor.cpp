2a24b0a0cb426f7851fe15d00741ccba577a8221
khulud-07 <khulud.harun@gmail.com>
2022-05-12 14:43:06 +0600

    nfkww

---
+++ CodeForces/STRING VECtor.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main ()
+{
+
+    int n, m;
+    cin >> n >> m;
+    if (m >= n)
+    {
+        if (n % 2 == 0)
+        {
+            cout << "Malvika" << endl;
+        }
+        else
+        {
+            cout << "Akshat" << endl;
+        }
+    }
+    else
+    {
+        if (n % 2 == 0)
+        {
+            cout << "Malvika" << endl;
+        }
+        else
+        {
+            cout << "Akshat" << endl;
+        }
+    }
+}
+
+
+
+
+
