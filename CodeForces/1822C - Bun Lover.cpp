766294b12085d17350997f5078df422fb890d4ef
Khulud Binte Harun <91691387+khulud-07@users.noreply.github.com>
2023-04-27 22:49:17 +0600

    Create 1822C - Bun Lover.cpp

---
+++ CodeForces/1822C - Bun Lover.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+      ll n;
+      cin>>n;
+      ll sol=(n*(n+2));
+      cout<<sol+2<<endl;
+
+    }
+
+}
+ 
